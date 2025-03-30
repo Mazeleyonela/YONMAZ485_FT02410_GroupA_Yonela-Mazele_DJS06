b0418b898591a4d46950eed3220f298890f6a0b2
Yonela Mazele <mazeleyonela8@gmail.com>
2025-03-30 13:30:51 +0200

    used map for uppercase and name lengths

--- scripts.js
+++ scripts.js
@@ -9,3 +9,12 @@ provinces.forEach(province => console.log(province));
 names.forEach((name, index) => {
   console.log(`${name} (${provinces[index]})`);
 });
+
+const uppercaseProvinces = provinces.map(province => province.toUpperCase());
+console.log(uppercaseProvinces);
+
+const nameLengths = names.map(name => name.length);
+console.log(nameLengths);
+
+const sortedProvinces = provinces.slice().sort();
+console.log(sortedProvinces);
